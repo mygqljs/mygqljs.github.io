d7a902766a78e8fad68a4c85c7ba8f1dd3b0691c
John Yuan <john.ycq@outlook.com>
2023-09-10 20:20:14 +0800

    chore: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports' }
+    ]
+  }
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+# vite-react-ts-starter
+
+Vite React TypeScript starter.
+
+## Commands
+
+```bash
+# install dependencies
+pnpm i
+
+# start dev server
+pnpm dev
+
+# format code with prettier
+pnpm format
+
+# lint code
+pnpm lint
+
+# lint code with auto fix
+pnpm lint:fix
+
+# format and lint staged files
+pnpm lint-staged
+```
+
+## Configured tools
+
+- [editorconfig](https://editorconfig.org/)
+- [eslint](https://eslint.org/)
+- [husky](https://github.com/typicode/husky)
+- [lint-staged](https://github.com/okonet/lint-staged)
+- [prettier](https://prettier.io/)
