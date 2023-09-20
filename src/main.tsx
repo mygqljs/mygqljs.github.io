7e8f295cd0d5ae731fff09d7518409a21b550662
John Yuan <john.ycq@outlook.com>
2023-09-20 11:05:59 +0800

    chore: init MyGQL pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div style={{ padding: '1rem' }}>Hello world!</div>
+    <div style={{ padding: '1rem' }}>Coming soon :)</div>
   </React.StrictMode>
 )
